26e650b0d001f1c68139df73b0aaa85f359bf99c
stefmitkovski <mitkovski.stefan.sm@gmail.com>
2023-08-12 15:16:30 +0200

    Add project files.

---
+++ DataBin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DataBin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
